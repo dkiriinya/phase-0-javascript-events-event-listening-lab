4b00938ec18cdca48abd8d6190a7437e43f1676a
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-08 09:58:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+    function addEventListener(eventName,callback){
+    }
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+    
 }
+
+addingEventListener()
